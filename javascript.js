ba52fe5964a4e9e7581c2c10094d9f2d7e2c0f15
mbright90 <michael.bright90@icloud.com>
2022-08-09 18:18:22 +0100

    Initiate recurring turns and button listeners

--- javascript.js
+++ javascript.js
@@ -1,4 +1,25 @@
-import {gameBoard, player} from './utilities/utilities.js'
+import {gameBoard, gameMaster} from './utilities/utilities.js'
+
+const hideModal = () => {
+    let modal = document.querySelector('.modal-background');
+    modal.style.visibility = 'hidden'
+
+    return hideModal;
+};
+
+let confirmButton = document.querySelector('.new-game-button')
+confirmButton.addEventListener('click', () => {
+    gameBoard.removeBoard();
+    gameBoard.clearStats();
+    gameBoard.createBoard();
+    gameMaster.playGame();
+    hideModal();
+});
+
+let cancelGameButton = document.querySelector('.cancel-game-button')
+cancelGameButton.addEventListener('click', () => {
+    hideModal();
+});
 
 gameBoard.createBoard();
-gameBoard.playerTurn();
+gameMaster.playGame();
