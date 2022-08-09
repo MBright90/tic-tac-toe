a997ea097972fbf3e698df61b0a1417b317ad338
mbright90 <michael.bright90@icloud.com>
2022-08-09 18:18:31 +0100

    Add further gameBoard functions
    Adding more functions to support the data flow from the beginning of a
    turn to the end. Implementing checks to show when the game is finished
    and feed this back to the players.

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,38 +1,68 @@
 const gameBoard = (() => {
 
-    let _activePlayer = 'X';
+    let _activePlayer = 'O';
+    let _turnLog = [];
+    let _gameComplete = false;
 
-    const _createGridSpaces = () => {
-        const gridPositionArr = ['1-1', '1-2', '1-3', '2-1', '2-2', '2-3', '3-1', '3-2', '3-3'];
-        let gridArr = [];
-        for (let positionIndex = 0; positionIndex < 9; positionIndex++) {
-            console.log(positionIndex)
-            const gridSpace = document.createElement('div');
-            gridSpace.classList.add('grid-space');
-            gridSpace.dataset.gridPosition = gridPositionArr[positionIndex];
-            gridArr.push(gridSpace);
-        }
-        return gridArr;
-    }
+    const _victoryConditions = [
+    ['1-1', '1-2', '1-3'],
+    ['2-1', '2-2', '2-3'],
+    ['3-1', '3-2', '3-3'],
+    ['1-1', '2-1', '3-1'],
+    ['1-2', '2-2', '3-2'],
+    ['1-3', '2-3', '3-3'],
+    ['1-1', '2-2', '3-3'],
+    ['1-3', '2-2', '3-1'] 
+    ];
 
     const _gridSpaceListeners = (nodeList) => {
         nodeList.forEach(space => {
-            space.addEventListener('click', () => {
-                space.textContent = gameMaster.activePlayer();
-                _nextTurn();
-            });
+            if (space.textContent === '') {
+                space.addEventListener('click', _placeTurn)
+            }
         });
     };
 
     const _removeSpaceListeners = (nodeList) => {
         nodeList.forEach(space => {
-            space.removeEventListener('click', () => {
-                space.textContent = gameMaster.activePlayer();
-                gameMaster._nextTurn();
-            });
+            space.removeEventListener('click', _placeTurn)
         });
+    };
+
+    // ---------- Game end functions ---------- //
+    const _checkVictoryStatus = () => {
+
+        let victory = false;
+
+        let filteredPlayer = _turnLog.filter((turn) => {
+            return turn.turnPlayer === _activePlayer ? turn.turnPosition : false;
+        });
+
+        let filteredTurns = filteredPlayer.map(log => log.turnPosition)
+
+        _victoryConditions.forEach(condition => {
+            if (filteredTurns.includes(condition[0]) &&
+                filteredTurns.includes(condition[1]) &&
+                filteredTurns.includes(condition[2])) {
+                    console.log('We have a winner')
+                    victory = true;
+            };
+        })
+        return victory;
+    };
+
+    const _checkGridFull = () => {
+        return _turnLog.length >= 9 ? true : false;
+    };
+
+    const _endGameMessage = (message) => {
+        let endGameModal = document.querySelector('.modal-background');
+        let messageSpace = document.querySelector('.end-game-message')
+        messageSpace.textContent = message;
+        endGameModal.style.visibility = 'visible';
     }
 
+    // ---------- Board functions ---------- //
     const createBoard = () => {
         if (!!document) {
             const gameArea = document.querySelector('.board-container');
@@ -42,70 +72,100 @@ const gameBoard = (() => {
             const gridArray = _createGridSpaces();
             gridArray.forEach(gridSpace => {
                 gameBoard.appendChild(gridSpace)
-                console.log(gameBoard);
             });
 
             gameArea.appendChild(gameBoard);
         }
-    }
+    };
+
+    const _createGridSpaces = () => {
+        const gridPositionArr = ['1-1', '1-2', '1-3', '2-1', '2-2', '2-3', '3-1', '3-2', '3-3'];
+        let gridArr = [];
+        for (let positionIndex = 0; positionIndex < 9; positionIndex++) {
+            const gridSpace = document.createElement('div');
+            gridSpace.classList.add('grid-space');
+            gridSpace.dataset.gridPosition = gridPositionArr[positionIndex];
+            gridArr.push(gridSpace);
+        };
+        return gridArr;
+    };
 
     const removeBoard = () => {
         if (!!document) {
             const gameBoard = document.querySelector('.game-board');
             gameBoard.remove();
         }
-    }
-
-    const activePlayer = () => {
-        return _activePlayer;
     };
 
-    const _endTurn = () => {
-        _removeSpaceListeners(document.querySelectorAll('.grid-space'))
-    }
+    // ---------- Turn functions ---------- //
 
-    const _nextTurn = () => {
+    const startNextTurn = () => {
         if (_activePlayer === 'X') {
             _activePlayer = 'O';
         } else {
             _activePlayer = 'X'
         };
-        _endTurn();
+        _gridSpaceListeners(document.querySelectorAll('.grid-space'));
     };
 
-    const playerTurn = () => {
-        _gridSpaceListeners();
+    const _placeTurn = (e) => {
+        e.target.textContent = _activePlayer;
+        let turnToLog = {
+            turnPosition: e.target.dataset.gridPosition,
+            turnPlayer: _activePlayer
+        }
+        _turnLog.push(turnToLog);
+        _endTurn();
     }
 
+    const _endTurn = () => {
+        _removeSpaceListeners(document.querySelectorAll('.grid-space'));
+        if (_checkVictoryStatus()) {
+            _gameComplete = true;
+            _endGameMessage(`${_activePlayer} wins!`);
+        } else if (_checkGridFull()) {
+            _gameComplete = true;
+            _endGameMessage('draw');
+        } else {
+            startNextTurn();
+        }
+    };
+
+    // const _setNextTurn = () => {
+    //     if (_activePlayer === 'X') {
+    //         _activePlayer = 'O';
+    //     } else {
+    //         _activePlayer = 'X'
+    //     };
+    //     startNextTurn();
+    // };
+
+    // ----------- Information functions ----------- //
+
+    const clearStats = () => {
+        _gameComplete = false;
+        _turnLog = [];
+    };
+
     return {
-        activePlayer: activePlayer,
-        createBoard,
-        removeBoard,
-        playerTurn,
+        createBoard: createBoard,
+        removeBoard: removeBoard,
+        startNextTurn: startNextTurn,
+        clearStats: clearStats
     };
 
 })();
 
-const player = (marker, playerNumber, activePlayer) => {
-    this._turnNumber = 0;
-    this._winsNumber = 0;
-    this._activePlayer = activePlayer;
+const gameMaster = (() => {
 
-    const numberOfTurns = () => {
-        return _turnNumber;
+    const playGame = () => {
+        gameBoard.startNextTurn()
     };
 
-    const numberOfWins = () => {
-        return _winsNumber
+    return {
+        playGame: playGame,
     };
 
-    return{
-        marker: marker, 
-        playerNumber: playerNumber,
-        numberOfTurns: numberOfTurns,
-        numberOfWins: numberOfWins,
-    }
+})();
 
-};
-
-export {gameBoard, gameMaster, player}
+export {gameBoard, gameMaster};
